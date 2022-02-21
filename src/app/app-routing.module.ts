057404985191947503ae0b3569f111075648aba7
AnaFranic <anafranic@outlook.com>
2022-02-21 21:55:33 +0100

    Added landing page and blogs list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogsListComponent } from './blogs-list/blogs-list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: LandingPageComponent,
+  },
+  {
+    path: 'blogs-list',
+    component: BlogsListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
