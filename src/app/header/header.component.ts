a13938e1ea11bc85b3dce3e5ea0ce2a591230bec
AnaFranic <anafranic@outlook.com>
2022-02-24 23:52:42 +0100

    Added header functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  animate = false;
+  stickyHeader = false;
+
+  private subscriptions = new Subscription();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.stickyHeader = window.scrollY > 0;
+    this.subscriptions.add(
+      fromEvent(window, 'scroll').subscribe(() => {
+        this.stickyHeader = window.scrollY > 0;
+      })
+    );
+    window.setTimeout(() => {
+      this.animate = true;
+    }, 50);
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
