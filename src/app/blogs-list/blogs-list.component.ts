0a1c856b882dd78960afceb824358a9cf47fc7c7
AnaFranic <anafranic@outlook.com>
2022-03-03 23:39:00 +0100

    Added blog list content

--- src/app/blogs-list/blogs-list.component.ts
+++ src/app/blogs-list/blogs-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Blog } from './blogs-list.models';
 
 @Component({
   selector: 'app-blogs-list',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blogs-list.component.scss']
 })
 export class BlogsListComponent implements OnInit {
+
+  blogs: Blog[] = [
+    {
+      title: 'Representation Matters: Inaugural Chubby Diaries Travel Awards',
+      subtitle: 'Representation Matters: Inaugural Chubby Diaries Travel Awards Chubby Diaries is happy to announce that we are presenting the first ever Inaugural Chubby Diaries Travel Awards, the',
+      image: './assets/img/blog-logo.png',
+      date: '1/3/2022'
+    },
+    {
+      title: 'Representation Matters: Inaugural Chubby Diaries Travel Awards',
+      subtitle: 'Representation Matters: Inaugural Chubby Diaries Travel Awards Chubby Diaries is happy to announce that we are presenting the first ever Inaugural Chubby Diaries Travel Awards, the',
+      image: './assets/img/blog-logo.png',
+      date: '2/3/2022'
+    }
+  ];
 
   constructor() { }
 
---
+++ src/app/blogs-list/blogs-list.models.ts
@@ -0,0 +1,6 @@
+export interface Blog {
+  title: string;
+  subtitle: string;
+  image: string;
+  date: string;
+}
