a0bcce51971b54025f0fb0a65e7e90ce54cd2b3e
AnaFranic <anafranic@outlook.com>
2022-03-10 00:07:57 +0100

    Add new blog

--- src/app/blogs-list/blogs-list.component.ts
+++ src/app/blogs-list/blogs-list.component.ts
@@ -17,6 +17,14 @@ export class BlogsListComponent implements OnInit, OnDestroy {
   constructor(private blogsService: BlogsService) { }
 
   ngOnInit(): void {
+    this.getBlogs();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private getBlogs(): void {
     this.subscriptions.add(
       this.blogsService.getBlogs().subscribe((blogs) => {
         this.blogs = blogs;
@@ -24,7 +32,19 @@ export class BlogsListComponent implements OnInit, OnDestroy {
     );
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
+  addNewBlog(): void {
+    const blog: Blog = {
+      id: 0,
+      title: 'Representation Matters: Inaugural Chubby Diaries Travel Awards',
+      subtitle: 'Representation Matters: Inaugural Chubby Diaries Travel Awards Chubby Diaries is happy to announce that we are presenting the first ever Inaugural Chubby Diaries Travel Awards, the',
+      image: './assets/img/blog-logo.png',
+      date: '3/3/2022',
+    };
+
+    this.subscriptions.add(
+      this.blogsService.addBlog(blog).subscribe((_) => {
+        this.getBlogs();
+      })
+    );
   }
 }
--- src/app/blogs-list/blogs.service.ts
+++ src/app/blogs-list/blogs.service.ts
@@ -6,12 +6,14 @@ import { Blog } from './blogs-list.models';
 export class BlogsService {
   private blogs: Blog[] = [
     {
+      id: 0,
       title: 'Representation Matters: Inaugural Chubby Diaries Travel Awards',
       subtitle: 'Representation Matters: Inaugural Chubby Diaries Travel Awards Chubby Diaries is happy to announce that we are presenting the first ever Inaugural Chubby Diaries Travel Awards, the',
       image: './assets/img/blog-logo.png',
       date: '1/3/2022'
     },
     {
+      id: 1,
       title: 'Representation Matters: Inaugural Chubby Diaries Travel Awards',
       subtitle: 'Representation Matters: Inaugural Chubby Diaries Travel Awards Chubby Diaries is happy to announce that we are presenting the first ever Inaugural Chubby Diaries Travel Awards, the',
       image: './assets/img/blog-logo.png',
@@ -22,7 +24,18 @@ export class BlogsService {
   constructor() { }
 
   getBlogs(): Observable<Blog[]> {
-    return of(this.blogs).pipe(delay(5000));
+    return of(this.blogs).pipe(delay(500));
+  }
+
+  addBlog(blog: Blog): Observable<Blog> {
+    const maxId = this.blogs.reduce((agg, curr) => agg < curr.id ? curr.id : agg, -1);
+    const newId = maxId + 1;
+    const blogCopy: Blog = {
+      ...blog,
+      id: newId,
+    };
+    this.blogs = [...this.blogs, blogCopy];
+    return of(blogCopy).pipe(delay(500));
   }
 }
 
